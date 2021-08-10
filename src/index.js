4e4cca23dc81449ba7cda4199122581964cd225b
alfredo <yoelvisjimenezr@gmail.com>
2021-08-09 23:37:59 -0500

    implementing authentication

--- src/index.js
+++ src/index.js
@@ -3,58 +3,34 @@ const { ApolloServer } = require('apollo-server');
 const fs = require('fs');
 const path = require('path');
 
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+const User = require('./resolvers/User');
+const Link = require('./resolvers/Link');
+
 const prisma = new PrismaClient();
 
 const resolvers = {
-    Query: {
-        info: () => `This is the API of a Hackernews Clone`,
-        feed: async (parent, args, context, info) => {
-            return context.prisma.link.findMany();
-        },
-        link: async (parent, args, context, info) => {
-            return context.prisma.link.findUnique({
-                where: { id: Number(args.id) },
-            });
-        },
-    },
-    Mutation: {
-        post: (parent, args, context, info) => {
-            const newLink = context.prisma.link.create({
-                data: {
-                    url: args.url,
-                    description: args.description,
-                }
-            });
-            return newLink;
-        },
-        updateLink: async (parent, args, context, info) => {
-            const link = await context.prisma.link.update({
-                where: {
-                    id: Number(args.id),
-                },
-                data: {
-                    description: args.description,
-                    url: args.url,
-                },
-            });
-            return link;
-        },
-        deleteLink: async (parents, args, context, info) => {
-            const link = await context.prisma.link.delete({
-                where: {
-                    id: Number(args.id),
-                },
-            });
-            return link;
-        },
-    }
+    Query,
+    Mutation,
+    User,
+    Link,
 };
+
+const { getUserId } = require('./utils');
 
 const server = new ApolloServer({
     typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8'),
     resolvers,
-    context: {
-        prisma,
+    context: ({ req }) => {
+        return {
+            ...req,
+            prisma,
+            userId:
+                req && req.headers.authorization
+                    ? getUserId(req)
+                    : null,
+        };
     },
 });
 
